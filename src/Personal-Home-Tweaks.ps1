7d315cc96d1b4f6a167323f61259a364e417d657
iRobie <github@doghairbakery.com>
2016-02-23 09:02:55 -0800

    Create Personal-Home-Tweaks.ps1

---
+++ src/Personal-Home-Tweaks.ps1
@@ -0,0 +1,6 @@
+## Add Handbrake to system path
+$AddedLocation ="C:\Program Files\Handbrake"
+$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+$OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+$NewPath= $OldPath + ';' + $AddedLocation
+Set-ItemProperty -Path "$Reg" -Name PATH –Value $NewPath
