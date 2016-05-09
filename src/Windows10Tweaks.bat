1eb830d3ab96e39bfd78c4d1763762b6f96ee34a
iRobie <github@doghairbakery.com>
2016-05-09 15:00:19 -0700

    Update Windows10Tweaks.bat

--- src/Windows10Tweaks.bat
+++ src/Windows10Tweaks.bat
@@ -1,7 +1,7 @@
 
 REM *** Hide the search box from taskbar. You can still search by pressing the Win key and start typing what you're looking for ***
 REM 0 = hide completely, 1 = show only icon, 2 = show long search box
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 1 /f
 
 REM *** Disable MRU lists (jump lists) of XAML apps in Start Menu ***
 ::reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackDocs" /t REG_DWORD /d 0 /f
