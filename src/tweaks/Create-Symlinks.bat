e77c4a996d06af08a1b45d8c52736036c077e289
iRobie <github@doghairbakery.com>
2016-05-12 13:40:09 -0700

    Update Create-Symlinks.bat

--- src/tweaks/Create-Symlinks.bat
+++ src/tweaks/Create-Symlinks.bat
@@ -1,4 +1,8 @@
 
-## Create Symbolic Links
+:: Add Dropbox to Documents folder
 cd %userprofile%\Documents
 mklink /d Dropbox %userprofile%\Dropbox
+
+:: ConEmu settings
+del "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml"
+mklink /H "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "%userprofile%\Dropbox\Programs\Settings\ConEmu.xml" 
