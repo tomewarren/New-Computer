31755c31e35afce161612dbc2f8e2354039d1fde
iRobie <github@doghairbakery.com>
2016-06-30 09:09:02 -0700

    Moved to onedrive

--- src/tweaks/Create-Symlinks.bat
+++ src/tweaks/Create-Symlinks.bat
@@ -1,8 +1,4 @@
 
-:: Add Dropbox to Documents folder
-cd %userprofile%\Documents
-mklink /d Dropbox %userprofile%\Dropbox
-
 :: ConEmu settings
 del "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml"
-mklink /H "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "%userprofile%\Dropbox\Programs\Settings\ConEmu.xml" 
+mklink /H "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "%userprofile%\OneDrive\Programs\Settings\ConEmu.xml" 
